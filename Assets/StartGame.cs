060fffa555348254d7b706bd1564f1bdb816bb66
Ritschie <vuchev7892@gmail.com>
2021-06-12 20:43:44 +0300

    fades and scene transitions

---
+++ Assets/StartGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour {
+    [SerializeField] private LevelLoader levelLoader;
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("TriggerEnter");
+        if (other.gameObject.GetComponent<Player>()) {
+            Debug.Log("TriggerEnter - Component");
+            levelLoader.LoadNextLevel();
+        }
+    }
+}
