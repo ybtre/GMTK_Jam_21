c6a5fbb5f271e33a4eb5befdcb6edc0fb5936a3e
Ritschie <vuchev7892@gmail.com>
2021-06-13 15:42:29 +0300

    introduction scene + scoring for props hitting ground

---
+++ Assets/GroundScoring.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundScoring : MonoBehaviour {
+    
+    [SerializeField] private int PropScore = 100;
+    [SerializeField] private ScoreManager scoreMngr;
+
+    private bool canAddToScore = true;
+    private void OnCollisionEnter(Collision other) {
+        if (other.collider.gameObject.layer == LayerMask.NameToLayer("Ground")) {
+            if (canAddToScore) {
+                Debug.Log("Prop: " + gameObject.name + " Collided with Ground");
+                scoreMngr.AddToScore(PropScore);
+                canAddToScore = false;
+            }
+        }
+    }
+}
