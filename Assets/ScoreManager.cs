1f463a82bc15eb186d59711f52b2439dbabd60bb
Ritschie <vuchev7892@gmail.com>
2021-06-13 16:56:20 +0300

    added score ui to level scene

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -1,10 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour {
    
    [SerializeField] private int TotalScore;
+   [SerializeField] private Text scoreText;
+
+   private void Update() {
+      scoreText.text = TotalScore.ToString();
+   }
 
    public void AddToScore(int amountToAdd) {
       TotalScore += amountToAdd;
