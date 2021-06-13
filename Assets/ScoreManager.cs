a50720dbb735d94bb1de04c38dbc35df47dab648
JGlend <1802879@uad.ac.uk>
2021-06-13 18:06:23 +0100

    Added text popup

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -8,13 +8,22 @@ public class ScoreManager : MonoBehaviour {
    
    [SerializeField] private int TotalScore;
    [SerializeField] private Text scoreText;
+    public GameObject FloatingText;
 
    private void Update() {
       scoreText.text = TotalScore.ToString();
    }
 
-   public void AddToScore(int amountToAdd) {
-      TotalScore += amountToAdd;
+   public void AddToScore(int amountToAdd, GameObject callingObject) {
+        TotalScore += amountToAdd;
+
+        if (FloatingText != null)
+        {
+            var objTransform = callingObject.transform;
+            var text = Instantiate(FloatingText, objTransform.position, Quaternion.identity);
+            text.GetComponent<TextMesh>().text = amountToAdd.ToString();
+        }
+
    }
 
    public void RemoveFromScore(int amountToRemove) {
